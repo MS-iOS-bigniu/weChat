1c163abaaba11c3b40e67fede0acc11c9cba8be4
mashun pc <mashun-pc@mashunpcdemacbook-pro.local>
2016-03-12 15:24:55 +0800

    创建了Person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by mashun－pc on 16/3/12.
+//  Copyright © 2016年 mashun－pc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by mashun－pc on 16/3/12.
+//  Copyright © 2016年 mashun－pc. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
